94796f04154b4af42e51bf4bc2a6b6b13c7edcaa
Swayam <swayamkerekar@gmail.com>
2026-04-26 08:18:15 +0530

    Deployment fix: Moved PHP files to api/ folder and updated vercel.json

--- router.php
+++ router.php
@@ -5,8 +5,20 @@ $uri = $_SERVER['REQUEST_URI'];
 $path = parse_url($uri, PHP_URL_PATH);
 $file = __DIR__ . $path;
 
-// If it's an .html file, process it as PHP
-if (pathinfo($path, PATHINFO_EXTENSION) === 'html' && file_exists($file)) {
+// If the path is just "/", look for index.php in api folder
+if ($path === '/' || $path === '/index.php') {
+    $file = __DIR__ . '/api/index.php';
+} else {
+    $file = __DIR__ . $path;
+}
+
+// If it's a PHP file and doesn't exist at root, check api folder
+if (pathinfo($path, PATHINFO_EXTENSION) === 'php' && !file_exists($file)) {
+    $file = __DIR__ . '/api' . $path;
+}
+
+// Process PHP files
+if (pathinfo($file, PATHINFO_EXTENSION) === 'php' && file_exists($file)) {
     include $file;
     return true;
 }
